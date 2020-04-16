e43f34e67e788e48753067173f220b8ba200ad4a
NanoTechzone <achintyaborah366@gmail.com>
2020-04-16 14:55:27 +0530

    HackingSetup

---
+++ hackingsetup.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+apt-get update -y   
+apt-get upgrade -y
+#update in progress
+apt install python -y           #installation......
+apt install python2 -y
+################################
+echo "We are Anonymous hackers ok"
+echo "My name is Nano"
+apt install ruby -y 
+apt install git -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+apt install bash -y
+apt install bmon -y
+echo "Programmed By Nano"
+apt install cmatrix
+apt install python3
+apt install mono
+pkg install curl -y
+pkg install unstable-repo
+pkg install wget -y
+pkg install openssh
+pkg install openssl
+
+
+
+
+
+
+
+toilet -f mono12 -F gay "Nano"
+apt install cmatrix
+cowsay "I m Foolish Cow"
+
+#starting................
+#Script is ready to accept......
+figlet I M Foolish
+echo "---------------------------------------------------------------------------------"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo  "#####################################"
+pkg install screenfetch -y
+screenfetch
+echo "Script by Nano"
+screenfetch
+screenfetch
+"##################################################"
+
+screenfetch
+
+screenfetch
+screenfetch
+
+screenfetch
+screenfetch
+
+screenfetch
+
+
+
+screenfetch
+
+screenfetch
+
+screenfetch
+
+screenfetch
+
+
+
+
+
+screenfetch
+echo "Created by Nano"
+screenfetch
+
+
+apt install libaca
+
+
+toilet -f mono12 -F border Hacking Setup
+
+echo "It's Nano's creation#######"
+
+
+
+
+echo "Here U go u had installed all the nessacery packages.........."
+
+
+
+echo "Now U can Hack And learn.......
+
+Now show your talent Of Hack3ing
+
+Now rock N roll"
+
+
+
+echo '
+>                         .--.    .--,
+>                         |  |   /  /
+>                         |  |  /  /
+>                         |  | /  /
+>                         |  |/  ;-._
+>                         }  ` _/  / ;
+>                         |  /` ) /  /
+>                         | /  /_/\_/\
+>                         |/  /      |
+>                         (    \  -  |
+>                          \    `.  /
+>                           |      |
+>                           |      |
+>                                    _
+>                           _ __  _ |_)
+>                          _> | |(_||v1.0 '|
+
+
+echo -e "$red                 ⫸$ylo U R Now Ready To Go$red ⫷$rset"
+
+
+
+
+
+
+
+
